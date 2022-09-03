da7ea1d8d17a2228091cda9d1ac52616819f2f08
Luke Brennan <lhbrennan@gmail.com>
2022-09-03 16:34:51 -0700

    cleanup Grid

--- src/components/VisualizerGrid.js
+++ src/components/VisualizerGrid.js
@@ -4,54 +4,33 @@ import { styled } from 'baseui';
 import { VisualizerCell } from './VisualizerCell';
 import { CENTER_WIDTH } from '../constants/layout';
 
-const Outer = styled('div', {
-  maxWidth: '650px',
-  margin: '0px auto',
+const Container = styled('div', {
   height: CENTER_WIDTH,
   width: CENTER_WIDTH,
-});
-
-const Middle = styled('div', {
-  paddingTop: '100%',
-  // overflow: 'hidden',
-  position: 'relative',
-});
-
-const Inner = styled('div', {
-  maxHeight: '700px',
-  maxWidth: '700px',
   display: 'grid',
   gridTemplateColumns: 'repeat(13, minmax(30px, 1fr))',
   gridTemplateRows: 'repeat(13, minmax(30px, 1fr))',
-
-  fontFamily: 'PT Sans, sans-serif',
-
   gridColumnGap: '4px',
   gridRowGap: '4px',
-
   borderRadius: '8px',
-
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  width: '100%',
-  height: '100%',
+  fontFamily: 'PT Sans, sans-serif',
 });
 
-export const VisualizerGrid = ({ hands, handStatusMap, handleStatusChange, pseudoSelectionMap }) => (
-  <Outer>
-    <Middle>
-      <Inner>
-        {hands.map((hand) => (
-          <VisualizerCell
-            label={hand}
-            status={handStatusMap[hand]}
-            handleStatusChange={handleStatusChange}
-            key={hand}
-            pseudoStatus={pseudoSelectionMap[hand]}
-          />
-        ))}
-      </Inner>
-    </Middle>
-  </Outer>
+export const VisualizerGrid = ({
+  hands,
+  handStatusMap,
+  handleStatusChange,
+  pseudoSelectionMap,
+}) => (
+  <Container>
+    {hands.map((hand) => (
+      <VisualizerCell
+        label={hand}
+        status={handStatusMap[hand]}
+        handleStatusChange={handleStatusChange}
+        key={hand}
+        pseudoStatus={pseudoSelectionMap[hand]}
+      />
+    ))}
+  </Container>
 );
--- src/constants/layout.js
+++ src/constants/layout.js
@@ -1,3 +1,3 @@
-export const CENTER_WIDTH = '500px';
+export const CENTER_WIDTH = '600px';
 export const LAYOUT_GRID_GUTTER = '32px';
 export const BUTTON_WIDTH = '168px';
