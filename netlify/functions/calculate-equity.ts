0ab0ad20ee89371efe027ee5c7233b0c182ef63d
Luke Brennan <lhbrennan@gmail.com>
2022-09-06 23:22:51 -0700

    install and test pokersolver

--- netlify/functions/calculate-equity.ts
+++ netlify/functions/calculate-equity.ts
@@ -10,11 +10,27 @@ event {
 */
 
 import { Handler } from '@netlify/functions';
+const Hand = require('pokersolver').Hand;
 
 const handler: Handler = async (event, context) => {
+  const hero = Hand.solve(['Ad', 'As', 'Jc', 'Ah', '2d', '3c', 'Kd']);
+  const villian = Hand.solve(['Ad', 'Ts', 'Kc', '7h', 'Qc', 'Qs', 'Qd']);
+  const winner = Hand.winners([hero, villian]);
+
+  let message;
+
+  if (winner.length > 1) {
+    message = `It's a tie! Hero has ${hero.name} and villian has ${villian.name}`;
+  }
+  if (winner[0] === hero) {
+    message = `Hero wins with ${hero.name}`;
+  } else if (winner[0] === villian) {
+    message = `Villian wins with ${villian.name}`;
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
+    body: JSON.stringify({ message }),
   };
 };
 
